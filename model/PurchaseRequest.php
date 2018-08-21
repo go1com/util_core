445a9e2b1a51bbbed447420c2d256a72a038e337
Loi Nguyen <loinguyentrung@gmail.com>
2018-08-21 11:55:20 +0700

    GO1P-20133 Added default value for purchase request

--- model/PurchaseRequest.php
+++ model/PurchaseRequest.php
@@ -2,30 +2,32 @@
 
 namespace go1\util\model;
 
+use go1\util\enrolment\PurchaseRequestStatus;
+
 class PurchaseRequest
 {
     /**
      * @var integer
      */
-    public $id;
+    public $id = 0;
 
     /**
      * @var array
      */
-    public $original;
+    public $original = [];
 
     /**
      * @var integer
      */
-    public $portalId;
+    public $portalId = 0;
 
     /**
      * @var integer
      */
-    public $status;
+    public $status = PurchaseRequestStatus::NOT_ACTIONED;
 
     /**
      * @var string
      */
-    public $responseDate;
+    public $responseDate = '';
 }
