5ed6d9695b6b7796d8b4af72269f301c6bb10b23
Luan Nguyen <luan.nguyen@go1.com>
2018-11-13 16:24:58 +0700

    GO1P-22377: add event.attendance.update_multiple

---
+++ lo/event_publishing/EventAttendanceUpdateMultiple.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace go1\util\lo\event_publishing;
+
+class EventAttendanceUpdateMultiple extends EventAttendanceCreate
+{
+    const ROUTING_KEY = 'event.attendance.update_multiple';
+}
--- lo/event_publishing/Events.php
+++ lo/event_publishing/Events.php
@@ -4,7 +4,8 @@ namespace go1\util\lo\event_publishing;
 
 class Events
 {
-    const EVENT_ATTENDANCE_CREATE = EventAttendanceCreate::ROUTING_KEY;
-    const EVENT_ATTENDANCE_UPDATE = EventAttendanceUpdate::ROUTING_KEY;
-    const EVENT_ATTENDANCE_DELETE = EventAttendanceDelete::ROUTING_KEY;
+    const EVENT_ATTENDANCE_CREATE           = EventAttendanceCreate::ROUTING_KEY;
+    const EVENT_ATTENDANCE_UPDATE           = EventAttendanceUpdate::ROUTING_KEY;
+    const EVENT_ATTENDANCE_UPDATE_MULTIPLE  = EventAttendanceUpdateMultiple::ROUTING_KEY;
+    const EVENT_ATTENDANCE_DELETE           = EventAttendanceDelete::ROUTING_KEY;
 }
