6d534c1f23fe76b76158098746e7e6583d23dd8e
Farhan Darma <farhan.darma@gmail.com>
2022-02-28 13:44:43 +1100

    CON-1402 Reverted PRO-3200 to remove LRS consume endpoint

--- queue/Queue.php
+++ queue/Queue.php
@@ -255,9 +255,6 @@ class Queue
     const USER_DOMAIN_PORTAL_ACCOUNT_CREATE = 'user-domain.portal-account.create';
     const USER_DOMAIN_PORTAL_ACCOUNT_UPDATE = 'user-domain.portal-account.update';
     const USER_DOMAIN_PORTAL_ACCOUNT_DELETE = 'user-domain.portal-account.delete';
-    
-    # lrs events
-    const LRS_EVENT_NEW                     = 'lrs.event.new'; // Receive learning record from external parties, body: {"event":"lrs", instanceId, oauth2ClientId, userId, payload}
 
     /**
      * @deprecated
