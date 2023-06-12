e8d7bdf24f49ee7b9e1264d9355e973ebd1b880f
David Nguyen <david.nguyen@go1.com>
2023-06-12 08:26:55 +0000

    COD-1885: add support DB persistent connection

---
+++ driver/PDOMySql/Driver.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace go1\util\Driver\PDOMySql;
+
+use go1\util\Driver\PDOConnection;
+use Doctrine\DBAL\Driver\PDOMySql\Driver as PDOMySQLDriver;
+use Doctrine\DBAL\Exception;
+use PDOException;
+
+/**
+ * @codeCoverageIgnore
+ */
+/**
+ * PDO MySql driver.
+ *
+ * @deprecated Use {@link PDO\MySQL\Driver} instead.
+ */
+class Driver extends PDOMySQLDriver
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function connect(array $params, $username = null, $password = null, array $driverOptions = [])
+    {
+        try {
+            $conn = new PDOConnection(
+                $this->constructPdoDsn($params),
+                $username,
+                $password,
+                $driverOptions
+            );
+        } catch (PDOException $e) {
+            throw Exception::driverException($this, $e);
+        }
+
+        return $conn;
+    }
+
+    /**
+     * Constructs the MySql PDO DSN.
+     *
+     * @param mixed[] $params
+     *
+     * @return string The DSN.
+     */
+    protected function constructPdoDsn(array $params)
+    {
+        $dsn = 'mysql:';
+        if (isset($params['host']) && $params['host'] !== '') {
+            $dsn .= 'host=' . $params['host'] . ';';
+        }
+
+        if (isset($params['port'])) {
+            $dsn .= 'port=' . $params['port'] . ';';
+        }
+
+        if (isset($params['dbname'])) {
+            $dsn .= 'dbname=' . $params['dbname'] . ';';
+        }
+
+        if (isset($params['unix_socket'])) {
+            $dsn .= 'unix_socket=' . $params['unix_socket'] . ';';
+        }
+
+        if (isset($params['charset'])) {
+            $dsn .= 'charset=' . $params['charset'] . ';';
+        }
+
+        return $dsn;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @deprecated
+     */
+    public function getName()
+    {
+        return 'pdo_mysql';
+    }
+}
--- tests/DBTest.php
+++ tests/DBTest.php
@@ -4,6 +4,7 @@ namespace go1\util\schema\tests;
 
 use go1\util\tests\MockPDO;
 use go1\util\DB;
+use go1\util\Driver\PDOMySql\Driver;
 use go1\util\tests\UtilCoreTestCase;
 use go1\util\user\UserHelper;
 use PDO;
@@ -33,6 +34,37 @@ class DBTest extends UtilCoreTestCase
 
         $_SERVER['REQUEST_METHOD'] = 'POST';
         $foo = DB::connectionOptions('foo', true);
+        $this->assertEquals('slave.foo.com', $foo['host']);
+    }
+
+    public function testPersistentConnectionOptions()
+    {
+        putenv('FOO_DB_NAME=foo_db');
+        putenv('FOO_DB_USERNAME=foo_username');
+        putenv('FOO_DB_PASSWORD=foo_password');
+        putenv('FOO_DB_SLAVE=slave.foo.com');
+        putenv('FOO_DB_HOST=foo.com');
+
+        $_SERVER['REQUEST_METHOD'] = 'POST';
+        $foo = DB::connectionPersistentOptions('foo');
+        $this->assertEquals('pdo_mysql', $foo['driver']);
+        $this->assertEquals('foo_db', $foo['dbname']);
+        $this->assertNotEquals('slave.foo.com', $foo['host']);
+        $this->assertEquals('foo_username', $foo['user']);
+        $this->assertEquals('foo_password', $foo['password']);
+        $this->assertEquals(Driver::class, $foo['driverClass']);
+        $this->assertEquals([
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4',
+            PDO::ATTR_PERSISTENT => true,
+        ], $foo['driverOptions']);
+        $this->assertEquals(3306, $foo['port']);
+
+        $_SERVER['REQUEST_METHOD'] = 'GET';
+        $foo = DB::connectionPersistentOptions('foo');
+        $this->assertEquals('slave.foo.com', $foo['host']);
+
+        $_SERVER['REQUEST_METHOD'] = 'POST';
+        $foo = DB::connectionPersistentOptions('foo', true);
         $this->assertEquals('slave.foo.com', $foo['host']);
     }
 
